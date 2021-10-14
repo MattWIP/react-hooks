4b50f9705b670e81165d7fdbaa26b530acb634bc
matt <matt@wip.fyi>
2021-10-14 15:49:51 -0700

    useToggle hook

---
+++ lib/useToggle.ts
@@ -0,0 +1,14 @@
+import { useCallback, useState } from 'react';
+
+const useToggle = (initialState = false) => {
+    // initialize the state
+    const [state, setState] = useState(initialState);
+
+    // Define and memorize toggler function in case we pass down the component,
+    // This function changes the boolean value to the opposite
+    const toggle = useCallback(() => setState(state => !state), []);
+
+    return [state, toggle];
+}
+
+export default useToggle;
